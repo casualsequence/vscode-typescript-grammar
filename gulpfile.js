e80460ee4e6fb1a2a2ca5ff5b3a3a3b59614b393
Caleb Cartwright <caleb.cartwright@outlook.com>
2016-08-20 23:38:09 -0500

    updated gulpfile to support local grammar json generation as well as pulling from the repo. updated npm scripts accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var download = require('gulp-download');
 var eslint = require('gulp-eslint');
 var jshint = require('gulp-jshint');
 var nsp = require('gulp-nsp');
 var path = require('path');
 var yaml = require('gulp-yaml');
+
+var grammarYAMLUrl = 'https://raw.githubusercontent.com/casualsequence/TypeScript-TmLanguage/master/TypeScript.YAML-tmLanguage';
 
 gulp.task('jshint', function() {
     return gulp.src('**/*.js')
@@ -24,7 +27,20 @@ gulp.task('nsp', function(cb) {
     nsp({package: path.resolve('package.json')}, cb);
 });
 
+// gulp.task('download-grammar-yaml')
+
+
 gulp.task('update-grammar', function() {
+    return download(grammarYAMLUrl)
+        .pipe(yaml({
+            schema: 'DEFAULT_FULL_SCHEMA',
+            space: 2,
+            filename: 'typescript.json'
+        }))
+        .pipe(gulp.dest('./syntaxes/'));
+});
+
+gulp.task('update-local-grammar', function() {
     return gulp.src('./TypeScript.yaml')
         .pipe(yaml({
             schema: 'DEFAULT_FULL_SCHEMA',
